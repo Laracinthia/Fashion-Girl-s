6adcb1de7715d9009514d9e8b104ac694a306ec3
Laracinthia <laracinthia1104@gmail.com>
2023-03-07 12:48:36 -0600

    07 de marzo
    diseño de pagina

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+    constructor(private cargarscripts: CargarscriptsService) {
+      cargarscripts.carga([
+        "assets/vendor/aos/aos.js",
+        "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+        "assets/vendor/glightbox/js/glightbox.min.js",
+        "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+        "assets/vendor/swiper/swiper-bundle.min.js",
+        "assets/vendor/php-email-form/validate.js",
+        "assets/js/main.js"
+      ])
+}
 }
--- src/app/content/ropadeportiva/ropadeportiva.component.ts
+++ src/app/content/ropadeportiva/ropadeportiva.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 @Component({
   selector: 'app-ropadeportiva',
   templateUrl: './ropadeportiva.component.html',
   styleUrls: ['./ropadeportiva.component.css']
 })
 export class RopadeportivaComponent {
-
+  
+    constructor(private cargarscripts: CargarscriptsService) {
+      cargarscripts.carga([
+        "assets/vendor/aos/aos.js",
+        "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+        "assets/vendor/glightbox/js/glightbox.min.js",
+        "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+        "assets/vendor/swiper/swiper-bundle.min.js",
+        "assets/vendor/php-email-form/validate.js",
+        "assets/js/main.js"
+      ])
 }
+}
+
