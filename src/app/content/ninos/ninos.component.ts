ccdae8e67efa61dc01e2a93b14dcfe2554868a6a
AylinRGlz <rangel.aylin2464276@gmail.com>
2023-03-07 12:49:51 -0600

    07 marzo 2023
    editar los componentes

--- src/app/content/ninos/ninos.component.ts
+++ src/app/content/ninos/ninos.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 @Component({
   selector: 'app-ninos',
   templateUrl: './ninos.component.html',
   styleUrls: ['./ninos.component.css']
 })
 export class NinosComponent {
+  constructor(private cargarscripts: CargarscriptsService) {
+    cargarscripts.carga([
+      "assets/vendor/aos/aos.js",
+      "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+      "assets/vendor/glightbox/js/glightbox.min.js",
+      "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+      "assets/vendor/swiper/swiper-bundle.min.js",
+      "assets/vendor/php-email-form/validate.js",
+      "assets/js/main.js"
+    ])
 
+}
 }
